3966925d313cf6497cb7613e3ad64539a57d34fe
Peter Lloyd <plloyd11@gmail.com>
2015-12-06 17:14:59 -0500

    added navigation animation

--- js/app.js
+++ js/app.js
@@ -1,1 +1,13 @@
 $(document).foundation();
+
+$("nav").headroom({
+  "offset": 50,
+  "tolerance": 5,
+  "classes": {
+    "pinned": "fadeIn",
+    "unpinned": "fadeOut"
+  }
+});
+
+// to destroy
+$("#header").headroom("destroy");
--- js/min/app-min.js
+++ js/min/app-min.js
@@ -1,2 +1,2 @@
-$(document).foundation();
+$(document).foundation(),$("nav").headroom({offset:50,tolerance:5,classes:{pinned:"fadeIn",unpinned:"fadeOut"}}),$("#header").headroom("destroy");
 //# sourceMappingURL=./app-min.js.map
